49efde509c544ff5d4856c5c3b56280fd9a0dc29
Andrea Amorosi <aamorosi@amazon.es>
2025-04-11 10:32:09 +0200

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,13 @@
 import { Logger } from '@aws-lambda-powertools/logger';
-import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
-import middy from '@middy/core';
-import type { Context } from 'aws-lambda';
 
 export const logger = new Logger({
   logBufferOptions: {
     enabled: true,
-    maxBytes: 3000000,
-    flushOnErrorLog: true,
+    maxBytes: 175,
   },
 });
 
-export const handler = middy()
-  .use(injectLambdaContext(logger, { flushBufferOnUncaughtError: true }))
-  .handler(async (event: { shouldFail: boolean }, context: Context) => {
-    logger.debug('processing', { requestId: context.awsRequestId });
-
-    if (event.shouldFail) {
-      try {
-        throw new Error('Simulated error');
-      } catch (error) {
-        logger.error('Error occurred', { error });
-        throw error;
-      }
-    }
-  });
+export const handler = async () => {
+  logger.debug('ok');
+  logger.debug('processing');
+};
